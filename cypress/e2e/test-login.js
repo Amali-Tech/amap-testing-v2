ce2af8cda06a3edaa31fa5348ea9262573a3454c
Jessica Lartey <jessica.lartey@amalitech.com>
2024-01-15 11:14:34 +0000

    reconfigure cucumber for new cypress project

---
+++ cypress/e2e/test-login.js
@@ -0,0 +1,15 @@
+const {When, Then, Given} = require("@badeball/cypress-cucumber-preprocessor")
+
+Given("a user visits the login page", () => {
+  cy.visit("https://amap.amalitech-dev.net/login");
+});
+
+When("the user enters their credentials", () => {
+  cy.get("#email").type('assessment.platform@amalitech.com');
+  cy.get("#password").type('Amap@123')
+  cy.get("#custom-button").click()
+});
+
+Then("the user is navigated to the dashboard", () => {
+  cy.url().should('eq', "https://amap.amalitech-dev.net/dashboard/home");
+});
