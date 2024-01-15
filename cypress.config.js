729a1f15fc7012fca5e109c837e5841ec2cc0825
Jessica Lartey <jessica.lartey@amalitech.com>
2024-01-15 10:45:40 +0000

    chore: reconfigure cypress for new project

--- cypress.config.js
+++ cypress.config.js
@@ -1,25 +1,9 @@
 const { defineConfig } = require("cypress");
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
-const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-
-async function setupNodeEvents(on, config) {
-  await preprocessor.addCucumberPreprocessorPlugin(on, config);
-
-  on(
-    "file:preprocessor",
-    createBundler({
-      plugins: [createEsbuildPlugin.default(config)],
-    })
-  );
-
-  return config;
-}
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "**/*.feature",
-    supportFile: false,
-    setupNodeEvents,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
