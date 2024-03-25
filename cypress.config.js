c36555a0ff597fb06dbf2c8aee9045b1e10ac2de
Ebenezer Ansere <ebenezer.ansere@amalitech.com>
2024-03-25 16:26:51 +0000

    fix: add new files

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,32 @@
-const { defineConfig } = require("cypress");
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
-const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-
+const { defineConfig } = require('cypress');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const {
+	addCucumberPreprocessorPlugin,
+} = require('@badeball/cypress-cucumber-preprocessor');
+const {
+	createEsbuildPlugin,
+} = require('@badeball/cypress-cucumber-preprocessor/esbuild');
 
 async function setupNodeEvents(on, config) {
-  await addCucumberPreprocessorPlugin(on, config);;
+	await addCucumberPreprocessorPlugin(on, config);
 
-  on(
-    "file:preprocessor",
-    createBundler({
-      plugins: [createEsbuildPlugin(config)],
-    })
-  );
+	on(
+		'file:preprocessor',
+		createBundler({
+			plugins: [createEsbuildPlugin(config)],
+		})
+	);
 
-  return config;
+	return config;
 }
 
 module.exports = defineConfig({
-  e2e: {
-    specPattern: "**/*.feature",
-    supportFile: false,
-    setupNodeEvents,
-  },
+	e2e: {
+		specPattern: '**/*.feature',
+		supportFile: 'cypress/support/e2e.js',
+		setupNodeEvents,
+		baseUrl: 'https://amap.amalitech-dev.net',
+		defaultCommandTimeout: 10000,
+		pageLoadTimeout: 180000,
+	},
 });
